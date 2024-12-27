9444cb64c5e0ae7e7d5553bdd15b355d7c6bb0be
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-27 00:08:12 -0300

    Add files via upload

---
+++ Estacionamento/PercorrerLista/js/index.js
@@ -0,0 +1,64 @@
+// const lista = document.querySelector("#lista");
+// const item = lista.querySelectorAll("p");
+// const listas = document.querySelector("#listas");
+const h1 = document.querySelector("#h1");
+
+/*function functionName() {
+ // Obter o conteúdo do elemento <label>
+ const label = document.getElementById("listas").innerText;
+
+ // Expressão regular para encontrar CPFs (sequências de 11 dígitos)
+ const regexCPF = /\b\d{11}\b/g;
+
+ // Extrair CPFs do texto
+ const cpfs = label.match(regexCPF);
+
+ // Criar um mapa para contar as ocorrências
+ const cpfCounts = cpfs.reduce((acc, cpf) => {
+  acc[cpf] = (acc[cpf] || 0) + 1;
+  return acc;
+ }, {});
+
+ // Separar os CPFs repetidos (mais de 2 ocorrências)
+ const repeatedCPFs = Object.keys(cpfCounts).filter(cpf => cpfCounts[cpf] > 2);
+
+ // Exibir todos os CPFs encontrados
+ console.log("Todos os CPFs:", cpfs);
+
+ // Exibir os CPFs com mais de 2 ocorrências
+ console.log("CPFs Repetidos (mais de 2 ocorrências):", repeatedCPFs);
+}*/
+
+// function functionName() {
+// // Obter o conteúdo do elemento <label>
+// const label = document.getElementById("listas").innerText;
+
+// // Expressão regular para encontrar CPFs (sequências de 11 dígitos)
+// const regexCPF = /\b\d{11}\b/g;
+
+// // Extrair CPFs do texto
+// const cpfs = label.match(regexCPF);
+// // Criar um mapa para contar as ocorrências
+// const cpfCounts = cpfs.reduce((acc, cpf) => {
+//   acc[cpf] = (acc[cpf] || 0) + 1;
+//   return acc;
+// }, {});
+
+// // Separar os CPFs repetidos (mais de 2 ocorrências)
+// const repeatedCPFs = Object.keys(cpfCounts).filter(cpf => cpfCounts[cpf] > 2);
+// // Exibir todos os CPFs encontrados
+// // console.log("Todos os CPFs:", cpfs);
+
+// // Exibir os CPFs com mais de 2 ocorrências
+// // console.log("CPFs Repetidos (mais de 2 registros com o mesmo CPF):", repeatedCPFs);
+
+// // Exibir a quantidade de vezes que cada CPF repetido foi encontrado
+// repeatedCPFs.forEach(cpf => {
+// /* 
+// console.log
+// */
+// alert
+// (`O CPF ${cpf} foi encontrado ${(cpfCounts[cpf])/2} em dois registros.`);
+// });
+
+// }
